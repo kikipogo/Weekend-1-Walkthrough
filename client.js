f3cddf1b77b4d3a79462a1eeacbcfac1cec70c24
Christine Pogatchnik <christinepogatchnik@Christines-MacBook-Pro.local>
2017-02-13 09:50:24 -0600

    CP: Adding new employee funtionality

---
+++ client.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){ //waits for DOM to completely load
+  $('#submitNewEmployee').on('click', function(){ //event listener on submitNewEmployee
+    // Declaring variables and retrieving values from input boxes
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var idNumber = $('#idNumber').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
+
+    //adds new employee row to the DOM
+    $('#employeeTableBody').append(
+      '<tr>' +
+      '<td>' + firstName + '</td>' +
+      '<td>' + lastName '</td>' +
+      '<td>' + idNumber + '</td>' +
+      '<td>' + jobTitle + '</td>' +
+      '<td>' + annualSalary + '</td>' +
+    '</tr>'
+    );
+  });
+});
